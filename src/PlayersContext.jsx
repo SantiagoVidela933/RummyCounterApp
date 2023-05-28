301cadc8238dfc6dce3aa507bda2d4abf8fb633e
SantiagoVidela933 <santi.videla.2@gmail.com>
2023-05-28 13:45:51 -0300

    Desarrollo Terminado.

--- src/PlayersContext.jsx
+++ src/PlayersContext.jsx
@@ -1,15 +1,19 @@
-import { createContext, useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { createContext, useState } from 'react';
 
 export const PlayersContext = createContext();
 
 export const PlayersProvider = ({ children }) => {
-
   const [player1Name, setPlayer1Name] = useState('');
   const [player1Score, setPlayer1Score] = useState(0);
-  
   const [player2Name, setPlayer2Name] = useState('');
   const [player2Score, setPlayer2Score] = useState(0);
+
+  const resetScores = () => {
+    setPlayer1Name('');
+    setPlayer1Score(0);
+    setPlayer2Name('');
+    setPlayer2Score(0);
+  };
 
   return (
     <PlayersContext.Provider
@@ -22,13 +26,10 @@ export const PlayersProvider = ({ children }) => {
         setPlayer2Name,
         player2Score,
         setPlayer2Score,
+        resetScores, // Incluye la función resetScores en el valor proporcionado al contexto
       }}
     >
       {children}
     </PlayersContext.Provider>
   );
-};
-
-PlayersProvider.propTypes = {
-  children: PropTypes.node.isRequired,
 };
--- src/components/Score/ScorePage.jsx
+++ src/components/Score/ScorePage.jsx
@@ -1,14 +1,15 @@
 import { PlayersContext } from '../../PlayersContext';
-import { useContext } from "react";
-
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
 
 import './ScorePage.css';
 
 export const ScorePage = () => {
+  const navigate = useNavigate();
 
   // Obtén los nombres y puntajes de los jugadores desde el contexto
-  const { player1Name, player1Score, player2Name, player2Score } = useContext(PlayersContext); 
-  
+  const { player1Name, player1Score, player2Name, player2Score, resetScores } = useContext(PlayersContext);
+
   // Determina el jugador con el puntaje más alto
   let winner = '';
   if (player1Score > player2Score) {
@@ -19,15 +20,29 @@ export const ScorePage = () => {
     winner = 'Empate';
   }
 
+  const handleRestart = () => {
+    // Reiniciar los puntajes de los jugadores
+    resetScores();
+
+    // Redirigir al usuario al homepage después de reiniciar el juego
+    navigate('/');
+    
+  };
+
   return (
     <div className="ScorePage">
+      <div className="ScorePage_title">
+        <h1 className="ScorePage_title-h1">RummyCounterApp</h1>
+      </div>
       <div className="ScorePage_popup">
         <h2>¡Partida Finalizada!</h2>
         <p>Jugador 1: {player1Name} - Puntaje: {player1Score}</p>
         <p>Jugador 2: {player2Name} - Puntaje: {player2Score}</p>
-        <p>¡El ganador es: {winner}!</p>
+        <p>El/La ganador es: {winner}!</p>
       </div>
+      <button className="ScorePage_restart-button" onClick={handleRestart}>
+        Reiniciar Juego
+      </button>
     </div>
-  )
-}
-
+  );
+};
